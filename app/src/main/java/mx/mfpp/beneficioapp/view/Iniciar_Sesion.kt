a92ea1004c041893f4bc0050802371827d584349
FernandaPat <ferpinpat05@gmail.com>
2025-10-08 17:36:06 -0600

    Redesign Iniciar_Sesion screen layout
    Refactored the Iniciar_Sesion composable to use Scaffold, improved layout with vertical scrolling, added input fields for email/phone and password, and updated navigation and UI elements for a more modern look. Added a preview composable for easier UI testing.

--- app/src/main/java/mx/mfpp/beneficioapp/view/Iniciar_Sesion.kt
+++ app/src/main/java/mx/mfpp/beneficioapp/view/Iniciar_Sesion.kt
@@ -1,49 +1,88 @@
 package mx.mfpp.beneficioapp.view
 
-import androidx.compose.foundation.border
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Card
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
-import mx.mfpp.beneficioapp.viewmodel.BeneficioJovenVM
+import androidx.navigation.compose.rememberNavController
+
 
 @Composable
 fun Iniciar_Sesion(navController: NavController, modifier: Modifier = Modifier) {
-    Card(
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(16.dp)
-            .border(
-                1.dp,
-                MaterialTheme.colorScheme.outline,
-                shape = MaterialTheme.shapes.medium
-            )
-    ) {
-        Box(
-            contentAlignment = Alignment.Center,
-            modifier = Modifier.fillMaxSize()
+    val scrollState = rememberScrollState()
+    Scaffold(
+        topBar = { ArrowTopBar(navController, "Iniciar Sesión") },
+    ) { paddingValues ->
+        Column(
+            modifier = modifier
+                .fillMaxSize()
+                .background(Color.White)
+                .verticalScroll(scrollState)
+                .imePadding()
+                .padding(paddingValues)
+                .padding(top = 3.dp)
         ) {
-            Column {
-                Text(
-                    text = "Pantalla Mapa Test",
-                    textAlign = TextAlign.Center,
-                    modifier = Modifier.fillMaxWidth()
-                )
-                TextButton(onClick = { navController.navigate("creditos") }) {
-                    Text(text = "Ir a créditos")
+            Etiqueta("Correo o número de teléfono",true)
+            CapturaTexto("Escribe aquí", 10)
+
+            Etiqueta("Contraseña", true)
+            CapturaTexto("Escribe aquí", 16)
+
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 20.dp, vertical = 80.dp),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center
+            ) {
+                BotonMorado(navController, "Iniciar Sesión", Pantalla.RUTA_INICIAR_SESION)
+
+                Spacer(Modifier.height(16.dp))
+                Row(
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Text("¿No tienes una cuenta? ", fontSize = 11.sp)
+
+                    Text(
+                        text = "Crear Cuenta",
+                        color = MaterialTheme.colorScheme.primary,
+                        textDecoration = TextDecoration.Underline,
+                        fontSize = 11.sp,
+                        modifier = Modifier.clickable {
+                            navController.navigate(Pantalla.RUTA_INICIAR_SESION)
+                        }
+                    )
                 }
             }
         }
+    }
+}
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun Iniciar_SesionPreview() {
+    MaterialTheme {
+        val navController = rememberNavController()
+        Iniciar_Sesion(navController)
     }
 }
