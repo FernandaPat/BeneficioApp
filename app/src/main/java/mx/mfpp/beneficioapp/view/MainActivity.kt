b6ece9a5a7f1da546187a4e777ef55fb13559d99
A01801092 <a01801092@tec.mx>
2025-10-22 15:59:18 -0600

    Remove password change page and update FAQ
    Deleted CambiarContrasenaPage and its navigation route from MainActivity. Expanded AyudaPage with more detailed FAQs about password recovery and other common issues. Updated AndroidManifest to enforce portrait orientation and adjust window input mode for MainActivity.

--- app/src/main/java/mx/mfpp/beneficioapp/view/MainActivity.kt
+++ app/src/main/java/mx/mfpp/beneficioapp/view/MainActivity.kt
@@ -228,9 +228,6 @@ fun AppNavHost(
         composable(Pantalla.RUTA_AYUDA_APP) {
             AyudaPage(navController)
         }
-        composable(Pantalla.RUTA_CAMBIARCONTRASENA_APP) {
-            CambiarContrasenaPage(navController)
-        }
         composable(Pantalla.RUTA_DATOSPERSONALES_APP) {
             VerDatosPersonalesPage(navController)
         }
